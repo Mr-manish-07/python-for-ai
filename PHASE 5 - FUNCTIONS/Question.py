f05dcb02244390ac9c489ace17407f4cee369e61
mr-mk-dev <itsmenishan.12@gmail.com>
2026-07-29 07:34:40 +0530

    Q 11 for functions

--- PHASE 5 - FUNCTIONS/Question.py
+++ PHASE 5 - FUNCTIONS/Question.py
@@ -97,18 +97,25 @@
 
 # 10. Write a function to generate the Fibonacci series up to N terms.
 
-n = int(input("Enter n : "))
-first = 0
-second = 1
-for i in range(0,n-2):
-    print(first)
-    temp = second
-    second = first+second
-    first = temp
+# n = int(input("Enter n : "))
+# first = 0
+# second = 1
+# for i in range(0,n-2):
+#     print(first)
+#     temp = second
+#     second = first+second
+#     first = temp
 
 #--------------------------------------------------------------------------------------#
 
 # 11. Write a function to check whether a number is a palindrome.
+num = int(input("Enter number : "))
+n = num
+rev = 0
+while n > 0 :
+    rev = (rev * 10 ) + (n % 10)
+    n = n//10
+print(num == rev)
 
 #--------------------------------------------------------------------------------------#
 
