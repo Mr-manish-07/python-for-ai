5f3e4a3bd73788d0cc67ae19b7457bf8c5a1384d
Manish-07 <manish825316>
2026-07-31 09:39:31 +0530

    Added new line

--- PHASE 5 - FUNCTIONS/Question.py
+++ PHASE 5 - FUNCTIONS/Question.py
@@ -109,21 +109,31 @@
 #--------------------------------------------------------------------------------------#
 
 # 11. Write a function to check whether a number is a palindrome.
-num = int(input("Enter number : "))
-n = num
-rev = 0
-while n > 0 :
-    rev = (rev * 10 ) + (n % 10)
-    n = n//10
-print(num == rev)
+
+# num = int(input("Enter number : "))
+# n = num
+# rev = 0
+# while n > 0 :
+#     rev = (rev * 10 ) + (n % 10)
+#     n = n//10
+# print(num == rev)
 
 #--------------------------------------------------------------------------------------#
 
-# 12. Write a function to calculate the sum of digits of a number.
+# 12. Write a function tao calculate the sum of digits of a number.
 
-#--------------------------------------------------------------------------------------#
+val = int(input("Enter value   : "))
+total = 0
+while val > 0 :
+    total = total + val % 10
+    val = val // 10
+print(total)
+
+#------------------------------------------------------------------------------------#
 
 # 13. Write a function to reverse the digits of a number.
+print("hello")
+
 
 #--------------------------------------------------------------------------------------#
 
